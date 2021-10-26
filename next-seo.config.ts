5bb12a78cc6a9d99789326b87d8e49775469810b
josueneneve <josue.neneve@hotmail.com>
2021-10-26 10:46:25 -0300

    Add NextSEO and NProgress

---
+++ next-seo.config.ts
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://my-trips.josueneneve.com.br/',
+    site_name: 'My Trips'
+  },
+  twitter: {
+    handle: '@qboacwb',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
