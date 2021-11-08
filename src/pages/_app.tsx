e2d5565a6b4ae9bfbbf0eaeb67ae1f5e387796a9
josueneneve <josue.neneve@hotmail.com>
2021-11-08 08:06:05 -0300

    Add google verification

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,10 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta
+          name="google-site-verification"
+          content="wrR6jgdCO8ulfFupu3EB7Mer-d74_8MFIqEhEoLOV6k"
+        />
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
